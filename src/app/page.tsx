60891796be0a4bf158375c77f7a88665e1920101
martinstorvoll <martin.storvoll@nav.no>
2024-08-15 12:55:04 +0200

    Lagt til dekoratøren

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,8 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { Button } from '@navikt/ds-react'
 
+import { Metadata } from 'next'
+
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Button>Send</Button>
-      <Button>Send</Button>
-      <Button>Send</Button>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta officia
-        tempora minima nulla natus molestias recusandae ipsa distinctio eius
-        corrupti!
-      </p>
-    </main>
-  )
+  return <main></main>
 }
