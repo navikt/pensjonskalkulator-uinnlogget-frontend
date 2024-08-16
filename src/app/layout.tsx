9b424403a1a13c4a8bb24cb88f2aa7907f6ffe96
martinstorvoll <martin.storvoll@nav.no>
2024-08-16 16:04:59 +0200

    Error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import '@navikt/ds-css'
 import { fetchDecoratorHtml } from '@navikt/nav-dekoratoren-moduler/ssr'
-import { extractScripts } from '@/helpers/Scriptextractor'
 import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
