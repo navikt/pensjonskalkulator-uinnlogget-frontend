fb70505ab814d26118f023e351718eacd6a0fd35
martinstorvoll <martin.storvoll@nav.no>
2024-08-20 14:28:58 +0200

    Added ref for child components

--- src/components/pages/InntektStep.tsx
+++ src/components/pages/InntektStep.tsx
@@ -1,23 +1,49 @@
-import React, { useContext, useState } from 'react'
+import React, {
+  forwardRef,
+  useContext,
+  useImperativeHandle,
+  useState
+} from 'react'
 import FormWrapper from '../FormWrapper'
-import { TextField } from '@navikt/ds-react'
+import { Box, TextField } from '@navikt/ds-react'
 import addState from '@/helpers/addState'
 import { FormContext } from '@/contexts/context'
-import { ContextForm } from '@/common'
+import { ContextForm, StepRef } from '@/common'
 
-function InntektStep() {
+const InntektStep = forwardRef<StepRef>((props, ref) => {
   const { states, setState } = useContext(FormContext) as ContextForm
+  const [errorMsg, setErrorMsg] = useState<string | null>(null)
+
+  useImperativeHandle(ref, () => ({
+    onSubmit() {
+      if (!states.inntekt?.state) {
+        setErrorMsg('Du må fylle ut inntekt')
+        return false
+      }
+
+      // Must not be negative
+      if (parseInt(states.inntekt.state) < 0) {
+        setErrorMsg('Inntekt kan ikke være negativ')
+        return false
+      }
+
+      return true
+    }
+  }))
 
   return (
     <FormWrapper>
       <div>Hva er din forventet inntekt?</div>
-      <TextField
-        onChange={(val) => addState(val.target.value, setState, 'inntekt')}
-        label='Inntekt'
-        value={states.inntekt?.state || ''}
-      />
+      <div className='w-24'>
+        <TextField
+          onChange={(val) => addState(val.target.value, setState, 'inntekt')}
+          label='Inntekt'
+          value={states.inntekt?.state || ''}
+          error={errorMsg}
+        />
+      </div>
     </FormWrapper>
   )
-}
+})
 
 export default InntektStep
