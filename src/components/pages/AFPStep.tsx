441eb75e522430d3b46726b9448607b9dbc4def6
9maPiltan <nima.piltan@nav.no>
2024-08-22 09:01:57 +0200

    updated afpstep

--- src/components/pages/AFPStep.tsx
+++ src/components/pages/AFPStep.tsx
@@ -22,7 +22,15 @@ const AFPStep = forwardRef<StepRef, FormPageProps>(({ grunnbelop }, ref) => {
   return (
     <FormWrapper>
       <Substep>
-        <RadioGroup legend={'Har du rett til AFP i privat sektor?'}>
+        <RadioGroup legend={'Har du rett til AFP i privat sektor?'}
+        value={states.rettTilAfp}
+        onChange={(it) =>
+          setState((prev: FormValues) => ({
+            ...prev,
+            rettTilAfp: it,
+          }))
+        }
+        >
           <Radio value='ja'>Ja</Radio>
           <Radio value='nei'>Nei</Radio>
         </RadioGroup>
@@ -32,6 +40,13 @@ const AFPStep = forwardRef<StepRef, FormPageProps>(({ grunnbelop }, ref) => {
           legend={`Har du ektefelle, partner eller samboer som har inntekt større enn ${
             2 * grunnbelop
           }kr når du starter å ta ut pensjon?`}
+          value={states.tredjepersonStorreEnn2G}
+          onChange={(it) =>
+            setState((prev: FormValues) => ({
+              ...prev,
+              tredjepersonStorreEnn2G: it,
+            }))
+          }
         >
           <Radio value='ja'>Ja</Radio>
           <Radio value='nei'>Nei</Radio>
@@ -41,6 +56,13 @@ const AFPStep = forwardRef<StepRef, FormPageProps>(({ grunnbelop }, ref) => {
         <RadioGroup
           legend={
             'Har du ektefelle, partner eller samboer som mottar pensjon eller uføretrygd fra folketrygden eller AFP når du starter å ta ut pensjon?'
+          }
+          value={states.tredjepersonMottarPensjon}
+          onChange={(it) =>
+            setState((prev: FormValues) => ({
+              ...prev,
+              tredjepersonMottarPensjon: it,
+            }))
           }
         >
           <Radio value='ja'>Ja</Radio>
