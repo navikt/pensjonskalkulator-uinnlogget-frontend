f79913cbab750a6cb8205215ae5ae98a084f943e
9maPiltan <nima.piltan@nav.no>
2024-09-10 12:09:42 +0200

    Bug fixed, another occured :)

--- src/app/api/simuler/route.ts
+++ src/app/api/simuler/route.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { requestAzureClientCredentialsToken } from "@navikt/oasis";
 
 const generateBearerToken = async (): Promise<string> => {
@@ -14,35 +14,40 @@ const generateBearerToken = async (): Promise<string> => {
         return clientCredentials.token;
     }
     else{
+        console.error("Failed to generate token: ", clientCredentials);
         throw new Error("Failed to generate token");
     }
 }
 
 //handler for å håndtere alle forespørsler
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-
+export async function POST(req: NextRequest/* req: NextApiRequest, res: NextApiResponse */) {
 
     if (req.method !== 'POST') {
-        console.log('Method not allowed');
-        return res.status(405).json({ message: 'Method not allowed' });
+        //return res.status(405).json({ message: 'Method not allowed' });
+        return NextResponse.json({ message: 'Method not allowed' });
     }
 
     const backendUrl = "https://pensjonskalkulator-backend.intern.dev.nav.no/api/v1/alderspensjon/anonym-simulering";
     
     try {
         const token = await generateBearerToken();
+        const reqBody = await req.json();
         const backendResponse = await fetch(backendUrl, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(req.body),
+            body: JSON.stringify(reqBody),
         });
 
         const data = await backendResponse.json();
-        res.status(backendResponse.status).json(data);
+        //res.status(backendResponse.status).json(data);
+        return NextResponse.json(data, { status: backendResponse.status });
     } catch (error) {
-        res.status(500).json({ error });
+        console.error(`Error in POST handler: ${(error as Error).message}`, error);
+        const errorObj: Error = error as Error;
+        //res.status(500).json({ error: errorObj.message });
+        return NextResponse.json({ error: errorObj.message }, { status: 500 });
     }
 }
