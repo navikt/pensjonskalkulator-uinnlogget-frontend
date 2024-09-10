ff68e27f6117d190a6705d65aa8e53822d8392b3
9maPiltan <nima.piltan@nav.no>
2024-09-10 11:26:59 +0200

    Created a middleware that sends creates and sends a bearer token to Azure AD

---
+++ src/app/api/simuler/route.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import { requestAzureClientCredentialsToken } from "@navikt/oasis";
+
+const generateBearerToken = async (): Promise<string> => {
+
+    const clientCredentials = await requestAzureClientCredentialsToken(
+        "api://dev-gcp:pensjonskalkulator:pensjonskalkulator-backend",
+    );
+
+    if(clientCredentials.ok){
+        return clientCredentials.token;
+    }
+    else{
+        throw new Error("Failed to generate token");
+    }
+}
+
+//handler for å håndtere alle forespørsler
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    const backendUrl = "https://pensjonskalkulator-backend.intern.dev.nav.no/api/v1/alderspensjon/anonym-simulering";
+    
+    try {
+        const token = await generateBearerToken();
+        const backendResponse = await fetch(backendUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(req.body),
+        });
+
+        const data = await backendResponse.json();
+        res.status(backendResponse.status).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
