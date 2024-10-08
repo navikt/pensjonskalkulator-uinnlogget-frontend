b35d07e7b7d7e02fac41d670fd7c56ea81046673
martinstorvoll <martin.storvoll@nav.no>
2024-10-08 19:05:00 +0200

    Linting error

--- src/components/__tests__/FormPage.test.tsx
+++ src/components/__tests__/FormPage.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import FormPage from '@/components/FormPage'
-import { FormContext } from '@/contexts/context'
-import { FormValues } from '@/common'
 
 // Mock the necessary modules and functions
 jest.mock('@/functions/submitForm', () => jest.fn())
