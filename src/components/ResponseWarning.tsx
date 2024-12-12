7f7dd7b5b490b3a159619008880cfcc4bcc040ad
martinstorvoll <martin.storvoll@nav.no>
2024-12-12 21:59:10 +0100

    Endret på error style

--- src/components/ResponseWarning.tsx
+++ src/components/ResponseWarning.tsx
@@ -1,8 +1,8 @@
 import { ErrorMessages, ErrorStatus, SimuleringError } from '@/common'
 import { FormContext } from '@/contexts/context'
 import { getErrors } from '@/texts/errors'
-import { Alert, BodyLong, Box, Button, Heading, VStack } from '@navikt/ds-react'
-import React, { useContext } from 'react'
+import { Alert, BodyLong, Box, Button, VStack } from '@navikt/ds-react'
+import { useContext } from 'react'
 
 interface SimuleringErrorProps {
   error?: SimuleringError
@@ -19,25 +19,30 @@ function ResponseWarning({ error }: SimuleringErrorProps) {
   }
 
   return (
-    <Box padding={'10'}>
-      <Alert variant="warning">
-        <Heading spacing size="small" level="3">
-          Viktig informasjon
-        </Heading>
-        <VStack gap="3">
-          <BodyLong spacing>{mapErrorToMessage(error)}</BodyLong>
-          <Box>
-            <Button
-              size="medium"
-              variant="secondary-neutral"
-              onClick={() => formPageProps.goTo(0)}
-            >
-              Endre
-            </Button>
-          </Box>
-        </VStack>
-      </Alert>
-    </Box>
+    <>
+      {/* <Heading level="1">Uinnlogget Pensjonskalkulator</Heading> */}
+      <Box
+        maxWidth={'40rem'}
+        width={'100%'}
+        marginInline={'auto'}
+        borderColor="border-default"
+      >
+        <Alert variant="warning">
+          <VStack gap="3">
+            <BodyLong spacing>{mapErrorToMessage(error)}</BodyLong>
+            <Box>
+              <Button
+                size="medium"
+                variant="secondary-neutral"
+                onClick={() => formPageProps.goTo(0)}
+              >
+                Endre
+              </Button>
+            </Box>
+          </VStack>
+        </Alert>
+      </Box>
+    </>
   )
 }
 
