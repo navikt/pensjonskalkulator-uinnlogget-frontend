102473f54bb9175bf104724297130cdd08a9e36a
9maPiltan <nima.piltan@nav.no>
2024-12-13 14:34:34 +0100

    Startside basert på UX tilbakemelding

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,40 +1,82 @@
-import { Box, Button, HStack, Link, VStack } from '@navikt/ds-react'
+'use client'
+
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Link,
+  VStack,
+  BodyLong,
+  List,
+} from '@navikt/ds-react'
+import stepStyles from './styles/stepStyles.module.css'
 import React from 'react'
+import Substep from './Substep'
 
-export default function QuestionBox() {
+export default function LandingPage() {
+  const Icon = ({ color }: { color: string }) => (
+    <Box
+      width="1rem"
+      height="1rem"
+      style={{ background: color }}
+      borderRadius="full"
+      aria-hidden
+    ></Box>
+  )
+
   return (
-    <Box width={'full'} background="surface-subtle">
-      <div className="flex flex-col items-center w-full">
-        <h2 className=" mb-3">Pensjonskalkulator</h2>
-
-        <Box
-          marginBlock={'auto'}
-          width={'100%'}
-          maxWidth={'40rem'}
-          padding={'4'}
-          background="bg-default"
-        >
-          <VStack padding={'6'} gap={'5'}>
-            <h3>Hei Du</h3>
-            <p>Velkommen til pensjonskalkulatoren som kan vise deg:</p>
-            <div>
-              <p>Alderspensjon (NAV)</p>
-              <p>AFP (avtalefestet pensjon)</p>
-              <p>Pensjonsavtaler</p>
-            </div>
-            <p>
-              For å beregne pensjonen din, må du svare på alle spørsmålene som
-              kommer.
-            </p>
-            <HStack gap={'2'}>
-              <Link href="./kalkulator-uinnlogget/form" className="text-white">
-                <Button variant="primary">Kom i gang</Button>
-              </Link>
-              <Button variant="tertiary">Avbryt</Button>
-            </HStack>
-          </VStack>
-        </Box>
-      </div>
+    <Box className={stepStyles.centerBox}>
+      <Box
+        marginBlock={'auto'}
+        width={'100%'}
+        maxWidth={'40rem'}
+        padding={'4'}
+        className={stepStyles.footerSpacing}
+      >
+        <Heading level="2" size="large" className={stepStyles.overskrift}>
+          Uinnlogget pensjonskalkulator
+        </Heading>
+        <VStack gap={'5'}>
+          <Heading level="2" size="medium">
+            Hei!
+          </Heading>
+          <BodyLong size="large">
+            Velkommen til forenklet pensjonskalkulator som kan gi deg et estimat
+            på:
+          </BodyLong>
+          <List as="ul">
+            <List.Item icon={<Icon color="var(--a-deepblue-500)" />}>
+              alderspensjon (Nav)
+            </List.Item>
+            <List.Item icon={<Icon color="var(--a-purple-400)" />}>
+              AFP i privat sektor (avtalefestet pensjon)
+            </List.Item>
+          </List>
+          <BodyLong size="large">
+            For å beregne pensjonen din, må du svare på alle spørsmålene som
+            kommer
+          </BodyLong>
+          <BodyLong size="small">
+            Du oppgir alle opplysninger selv. Vi ber ikke om personopplysninger
+            som kan identifisere deg. Ingen opplysninger lagres.
+          </BodyLong>
+          <HStack gap={'2'}>
+            <Link href="./kalkulator-uinnlogget/form" className="text-white">
+              <Button variant="primary">Kom i gang</Button>
+            </Link>
+            <Button variant="tertiary">Avbryt</Button>
+          </HStack>
+        </VStack>
+        <Substep>
+          <BodyLong size="large">
+            Kan du logge inn på Nav, anbefaler vi&nbsp;
+            <Link href="https://www.nav.no/pensjon/kalkulator/login">
+              innlogget kalkulator
+            </Link>
+          </BodyLong>
+        </Substep>
+      </Box>
     </Box>
   )
 }
