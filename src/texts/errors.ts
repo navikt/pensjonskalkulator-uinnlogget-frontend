051427bf6e11e08fdf7252ce9565e79f0ea03146
martinstorvoll <martin.storvoll@nav.no>
2024-12-16 11:16:15 +0100

    Endret tekst til feilmelding

--- src/texts/errors.ts
+++ src/texts/errors.ts
@@ -4,9 +4,9 @@ export const errors = {
   PKU222BeregningstjenesteFeiletException:
     'Det har oppstått en feil. Vennligst prøv igjen senere.',
   PKU225AvslagVilkarsprovingForLavtTidligUttakException:
-    'Opptjeningen din er ikke høy nok til ønsket uttak.',
+    'Opptjeningen din er ikke høy nok til ønsket uttak. Du må øke alderen eller sette ned uttaksgraden.',
   PKU224AvslagVilkarsprovingForKortTrygdetidException:
-    'Opptjeningen din er ikke høy nok til ønsket uttak.',
+    'Opptjeningen din er ikke høy nok til ønsket uttak. Du må øke alderen eller sette ned uttaksgraden.',
   default: 'Det har oppstått en feil. Vennligst prøv igjen senere.',
 } as ErrorMessages
 
