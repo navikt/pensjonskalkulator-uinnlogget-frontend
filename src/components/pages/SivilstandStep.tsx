3cd4bd6ebe85e8de2e18e3b5159021e5ac7667e9
9maPiltan <nima.piltan@nav.no>
2025-03-06 10:48:19 +0100

    PEK-1103

--- src/components/pages/SivilstandStep.tsx
+++ src/components/pages/SivilstandStep.tsx
@@ -72,28 +72,6 @@ const SivilstandStep = ({ grunnbelop }: FormPageProps) => {
         <>
           <Substep>
             <RadioGroup
-              legend={`Har din ektefelle, partner eller samboer inntekt større enn ${
-                grunnbelop ? `${formatInntekt(grunnbelop)} kr` : '2G'
-              } når du starter å ta ut pensjon?`}
-              defaultValue={state.epsHarInntektOver2G}
-              onChange={(it: boolean) =>
-                handleFieldChange((draft) => {
-                  draft.epsHarInntektOver2G = it
-                }, 'epsHarInntektOver2G')
-              }
-              error={errorFields.epsHarInntektOver2G}
-            >
-              <Radio
-                data-has-error={errorFields.epsHarInntektOver2G ? true : false}
-                value={true}
-              >
-                Ja
-              </Radio>
-              <Radio value={false}>Nei</Radio>
-            </RadioGroup>
-          </Substep>
-          <Substep>
-            <RadioGroup
               legend={
                 'Har din ektefelle, partner eller samboer pensjon eller uføretrygd fra folketrygden (Nav) eller AFP når du starter å ta ut pensjon?'
               }
@@ -107,6 +85,28 @@ const SivilstandStep = ({ grunnbelop }: FormPageProps) => {
             >
               <Radio
                 data-has-error={errorFields.epsHarPensjon ? true : false}
+                value={true}
+              >
+                Ja
+              </Radio>
+              <Radio value={false}>Nei</Radio>
+            </RadioGroup>
+          </Substep>
+          <Substep>
+            <RadioGroup
+              legend={`Har din ektefelle, partner eller samboer inntekt større enn ${
+                grunnbelop ? `${formatInntekt(grunnbelop)} kr` : '2G'
+              } når du starter å ta ut pensjon?`}
+              defaultValue={state.epsHarInntektOver2G}
+              onChange={(it: boolean) =>
+                handleFieldChange((draft) => {
+                  draft.epsHarInntektOver2G = it
+                }, 'epsHarInntektOver2G')
+              }
+              error={errorFields.epsHarInntektOver2G}
+            >
+              <Radio
+                data-has-error={errorFields.epsHarInntektOver2G ? true : false}
                 value={true}
               >
                 Ja
