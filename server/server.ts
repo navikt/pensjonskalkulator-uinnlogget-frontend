a766bf2dbf246e0aa98964ee3393a3050e370d43
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-05-23 10:54:35 +0200

    Steng for G-regulering med express backend

---
+++ server/server.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import next from 'next'
+
+import { stengForReguleringMiddleware } from '@navikt/steng-for-regulering/express'
+
+const isDev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 8080
+const app = next({ dev: isDev })
+const handle = app.getRequestHandler()
+const server = express()
+
+app
+  .prepare()
+  .then(() => {
+    server.use(stengForReguleringMiddleware({ env: isDev ? 'dev' : 'prod' }))
+    server.get(/(.*)/, (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(PORT, () => {
+      console.log(`Server ready on port ${PORT}`)
+    })
+  })
+  .catch((exception) => {
+    console.error(exception.stack)
+    process.exit(1)
+  })
